5122dfe2eacfb1dedfe4d6a1bca1b93b03f2d2be
mtlr05 <davidmay44@hotmail.com>
2022-01-20 13:25:16 -0500

    typo

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='finaigle',
-    url='https://github.com/mtlr05/finaigle',
+    name='finagle',
+    url='https://github.com/mtlr05/finagle',
     packages=find_packages(include=['finagle', 'finagle.*']),
     install_requires=['numpy','pandas'],
     description='for the valuation of a company',
