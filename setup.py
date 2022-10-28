afeaf52d850172303ee19b567ffe822c2bd85f59
mtlr05 <50175030+mtlr05@users.noreply.github.com>
2022-10-28 09:11:42 -0400

    github actions

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name='finagle',
     url='https://github.com/mtlr05/finagle',
     packages=find_packages(include=['finagle', 'finagle.*']),
-    install_requires=['numpy','pandas','xlrd','datetime','openpyxl','scipy','logging'],
+    install_requires=['numpy','pandas','xlrd','datetime','openpyxl','scipy'],
     description='for the valuation of a company',
 )
